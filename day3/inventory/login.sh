5babcc7d2bb0a93fdc134461833080235315df8f
vagrant <vagrant@vagrant.vm>
2017-09-03 23:26:04 +0000

    complete login page

--- day3/inventory/login.sh
+++ day3/inventory/login.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+clear
+
+dologin(){
+
+uname="admin"
+pass="123"
+
+if [ $uname == "$1" ] && [ $pass == "$2" ]
+then
+	./dashboard.sh $1
+else
+	echo "invalid user name or password"
+	exit
+fi
+
+}
+
 
 echo "***************************Login Page********************************"
+
+
+
+read -p "Enter user name :" ename
+read -p "Enter password :" epass
+
+
+dologin $ename $epass
+
+
