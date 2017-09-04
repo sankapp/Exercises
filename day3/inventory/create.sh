ce658c183cac53b57d120e2f5b3374be5b23b723
vagrant <vagrant@vagrant.vm>
2017-09-04 00:40:01 +0000

    complete create item with grep

--- day3/inventory/create.sh
+++ day3/inventory/create.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 clear
 echo "**********************************Create Page**********************************"
-echo $1
+
+createItem(){
+
+read -p "Enter Item Name :" iname
+read -p "Enter Item Category :" icat
+read -p "Enter Item Shop :" ishop
+read -p "Enter Item Price :" iprice
+
+if grep "^[1]" item.txt
+then
+	no=$(grep -c "^[0-9]*" item.txt)
+	((no++))
+	echo $no"). Item Name :-"$iname "-----Category :-"$icat "-----Available shop :-"$ishop "-----Price :-"$iprice >> item.txt
+	
+else
+	echo "1). Item Name :-"$iname "-----Category :-"$icat "-----Available shop :-"$ishop "-----Price :-"$iprice >> item.txt
+
+fi
+
+}
+
+createItem
