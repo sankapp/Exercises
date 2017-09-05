d9be4ada9e7c5237ff8194f8db8d57aa8c2b969e
vagrant <vagrant@vagrant.vm>
2017-09-05 04:29:44 +0000

    complete view and separate view

--- day3/inventory/view.sh
+++ day3/inventory/view.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 clear
+viewItem(){
+
+read -p "Enter item no to view separate view :" ino
+clear
+echo "---------------------------------Item Separate View-----------------------------"
+grep "^[$ino]" item.txt
+echo""
+echo "--------------------------------------------------------------------------------"
+
+}
 echo "*********************************View Item*************************************"
-echo $1
+
+cat item.txt
+
+viewItem
